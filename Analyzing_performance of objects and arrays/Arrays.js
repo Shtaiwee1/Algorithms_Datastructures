29bdee048521f84d6a0e23d270b282f2e3293473
Shtaiwee <shtaiweeomair@gmail.com>
2022-05-16 21:55:41 +0300

    algo

---
+++ Analyzing_performance of objects and arrays/Arrays.js
@@ -0,0 +1,16 @@
+let names = ["Michael","Melissa", "Andrea"];
+
+//Insertion and removal big O notation depend on where you want to insert or remove the element
+//So if you want to insert or remove at the end of an array it's O(1) but in the beginning of an array it's O(n) Because you have to reindex every single element in the array
+//Searching O(n)
+//Access O(1)
+
+//push O(1)
+//pop O(1)
+//shift O(n)
+//unshift O(n)
+//concat O(n)
+//sliceO(n)
+//splice O(n)
+//sortO(nlogn)
+//map/filter/reduce/foreach...etc  O(n)
---
+++ Analyzing_performance of objects and arrays/Objects.js
@@ -0,0 +1,17 @@
+let instructor ={
+    firstName : "kelly",
+    isInstructor: "true",
+    favoriteNumbers: [1,2,3,4]
+}
+
+
+
+//adding keys or values to an object : O(1)
+//removing :O(1)
+//Accessing: O(1)
+//Searching:O(n)
+
+//object.keys(name of the object)  O(n)
+//object.values(name of the object) O(n)
+//object.entries(name of the object) O(n)
+//object.hasOwnProperty(name of keys)  O(1)
